aba0e76a7a63edeefd64fde301e9562b2cf9bcd6
svante.kumlien@gmail.com <svante.kumlien@gmail.com>
2017-01-01 21:53:11 +0100

    add the manifest.yml

--- src/main/java/se/kumliens/concept2runkeeper/configuration/MailCloudConfiguration.java
+++ src/main/java/se/kumliens/concept2runkeeper/configuration/MailCloudConfiguration.java
@@ -1,7 +1,5 @@
 package se.kumliens.concept2runkeeper.configuration;
 
-import org.springframework.cloud.Cloud;
-import org.springframework.cloud.CloudFactory;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.cloud.config.java.ServiceScan;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +8,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.mail.MailSender;
 
 /**
+ * Provide a MailSender when running in CF (the SendGrid service)
+ *
  * Created by svante2 on 2017-01-01.
  */
 @Profile("cloud")
